88aa7eba05e3a01a0732231487fc2d48e3f78fa9
katarzyna-kw <katarzyna.koch@gmail.com>
2022-05-08 15:49:14 -0500

    created ShortenerSerializer, migrations to db

---
+++ url_shortener_app/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Shortener
+
+
+class ShortenerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shortener
+        fields = ["id", "long_url", "short_url"]
+    
+    long_url = serializers.URLField()
+    short_url = serializers.CharField(allow_null=True, read_only=True)
