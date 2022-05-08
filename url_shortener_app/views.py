38801a6dd967ad45d7cd243907215a3c373f0fe1
katarzyna-kw <katarzyna.koch@gmail.com>
2022-05-08 15:58:41 -0500

    created Shortener Viewset, added urls.py, able to add instance at /encode endpoint

--- url_shortener_app/views.py
+++ url_shortener_app/views.py
@@ -1,3 +1,12 @@
+from .models import Shortener
+from .serializers import ShortenerSerializer
+from rest_framework.viewsets import ModelViewSet
+from .utils import BASE_URL
 from django.shortcuts import render
 
-# Create your views here.
+class ShortenerViewSet(ModelViewSet):
+    queryset = Shortener.objects.all()
+    serializer_class = ShortenerSerializer
+    
+    def get_queryset(self):
+        return Shortener.objects.all().order_by("-id")
